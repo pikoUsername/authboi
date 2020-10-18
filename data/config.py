0ecf93cae3126b2ee08f7f4abcbc477e866734c7
CrimsonTod <galymzhan.amantaj@gmail.com>
2020-10-18 15:26:06 +0600

    2 nd commit

--- data/config.py
+++ data/config.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-DB_NAME =
-DB_HOST =
-DB_PORT =
+DB_NAME = str(os.getenv("DB_NAME"))
+DB_HOST = str(os.getenv("DB_HOST"))
+DB_PORT = str(os.getenv("DB_PORT"))
+DB_USER = str(os.getenv("DB_USER"))
+DB_PASS = str(os.getenv("DB_PASS"))
 
-
-
-DSN = ""
+DSN = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
