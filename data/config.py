fe634418f503ce68c5861ee4a567a891ccdcceca
CrimsonTod <galymzhan.amantaj@gmail.com>
2020-12-20 19:59:04 +0600

    update data/config, removed webhooks config

--- data/config.py
+++ data/config.py
@@ -20,19 +20,12 @@ POSTGRES_PORT = dint("DB_PORT")
 POSTGRES_USER = dstr("DB_USER")
 POSTGRES_PASS = dstr("DB_PASS")
 
-# webhook configs
-DOMAIN = dstr("DOMAIN", default="localhost")
-SECRET_KEY = secrets.token_urlsafe(48)
-WEBHOOK_BASE_PATH = dstr("WEBHOOK_BASE_PATH", default="/webhook")
-WEBHOOK_PATH = f"{WEBHOOK_BASE_PATH}:{SECRET_KEY}"
-WEBHOOK_URL = f"https://{DOMAIN}{WEBHOOK_PATH}"
-
+# telegram
 BOT_TOKEN = dstr("BOT_TOKEN")
 ADMIN_IDS = [
-    os.getenv("ADMIN_IDS"),
-    935770891, # dont delete pls
+    os.getenv("ADMIN_IDS")
 ]
-TELEGRAM_PORT = dint("TELEGRAM_PORT", default=8443)
+# logs path
 LOGS_BASE_PATH = str(Path(__name__).parent.parent / "logs")
-
+# uri idk what is it
 POSTGRES_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_NAME}"
