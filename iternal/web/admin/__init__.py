8be1664336008deeb837befc7d87718ad4238ae0
pikoUsername <galymzhan.amantaj@gmail.com>
2021-02-20 13:13:57 +0300

    i m not sure what i doing

--- iternal/web/admin/__init__.py
+++ iternal/web/admin/__init__.py
@@ -1,15 +1,18 @@
+import asyncio
 import aiohttp_jinja2
 from aiohttp import web
 import jinja2
-from typing import Union, List
+from typing import Union, List, Any
 from pathlib import Path
 
-from .consts import APP_KEY
+from .consts import APP_KEY, PROJ_ROOT
+from .handler import AdminHandler
 from .utils import gather_template_folders
 
 # based on https://github.com/aio-libs/aiohttp_admin/ or copy'n past
 __all__ = (
     "setup",
+    "get_admin"
 )
 
 
@@ -19,11 +22,33 @@ def setup(
     *,
     static_folder: Path = None,
     template_folder: Union[Path, List[Path]] = None,
-    app_key: str = APP_KEY
+    app_key: str = APP_KEY,
+    resources: Any = None,
+    template_name: str = None,
+    name: str = None,
+    loop: asyncio.AbstractEventLoop = None,
 ) -> None:
-    admin_app = web.Application()
-    app[app_key] = admin_app
+    assert prefix.startswith("/")
+
+    loop = loop or asyncio.get_event_loop()
+    admin = web.Application(loop=loop)  # need to test
+    app[app_key] = admin
 
     templatef = gather_template_folders(template_folder)
     loader = jinja2.FileSystemLoader(templatef)
-    aiohttp_jinja2.setup(admin_app, loader=loader, app_key=app_key)
+    aiohttp_jinja2.setup(admin, loader=loader, app_key=app_key)
+
+    template_name = template_name if template_name else "admin.html"
+    admin_handler = AdminHandler(admin, resources=resources, name=name,
+                                 template=template_name, loop=loop)
+
+    admin['admin_handler'] = admin_handler
+    admin['layout_path'] = prefix
+
+    static_folder = static_folder or str(PROJ_ROOT / 'static')
+    setup_admin_handlers(admin, admin_handler, static_folder, prefix)
+    return admin
+
+
+def get_admin(app: web.Application, *, app_key=APP_KEY):
+    return app.get(app_key, None)
--- iternal/web/admin/handler.py
+++ iternal/web/admin/handler.py
@@ -1,8 +1,25 @@
+from __future__ import annotations
+
+import sys
+
+import asyncio
 from aiohttp import web
 
 
 class AdminHandler(web.View):
-    __slots__ = ()
+    __slots__ = "app", "loop", "resources", "name"
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        app: web.Application,
+        loop: asyncio.AbstractEventLoop = None,
+        *, resources=None,
+        name: str = None
+    ) -> None:
+        self.app = app
+        self.loop = loop
+        self.resources = resources
+        self.name = name
+
+
+def setup_admin_handler(app: web.Application, handler)
