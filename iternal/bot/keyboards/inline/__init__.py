5096456c4cca45f6ad56da204555aff18e57e148
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-21 13:28:48 +0600

    using syntax sugar
    (...) -> ...

--- iternal/bot/keyboards/inline/__init__.py
+++ iternal/bot/keyboards/inline/__init__.py
@@ -2,4 +2,4 @@ from .start import get_choice_kb
 from .admin import get_admin_btns
 from .event import get_event_kb
 
-__all__ = ("get_admin_btns", "get_choice_kb", "get_event_kb")
+__all__ = "get_admin_btns", "get_choice_kb", "get_event_kb"
