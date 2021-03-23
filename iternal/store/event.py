1cb5492ca58260d140c2f1d9228476b25f92e6cf
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-23 12:58:17 +0600

    Changed baseclass for models, to TimedBaseModel
    password String lenfth was changed to 255

--- iternal/store/event.py
+++ iternal/store/event.py
@@ -1,21 +1,13 @@
-from sqlalchemy import sql
-
-from .base import db_
+from .base import db_, TimedBaseModel
 
 
-class Event(db_.Model):
+class Event(TimedBaseModel):
     """
     Uses for create Event
     """
     __tablename__ = "Event"
 
-    query: sql.Select
-
-    id = db_.Column(db_.Integer, primary_key=True, index=True, unique=True)
     text = db_.Column(db_.String)
     link_img = db_.Column(db_.String(300))
     inline_text = db_.Column(db_.String)
     inline_btn_link = db_.Column(db_.String(300))
-
-    def __str__(self):
-        return self.text
--- iternal/store/user.py
+++ iternal/store/user.py
@@ -1,29 +1,21 @@
-from sqlalchemy import sql
-
-from .base import db_
+from .base import db_, TimedBaseModel
 
 __all__ = "User",
 
 
-class User(db_.Model):
+class User(TimedBaseModel):
     """
     User need for store User model,
     uses for indentifi
     """
     __tablename__ = 'users'
 
-    id = db_.Column(db_.Integer, primary_key=True, index=True, unique=True)
     user_id = db_.Column(db_.BigInteger)
     username = db_.Column(db_.String(50))
     full_name = db_.Column(db_.String(100))
     login = db_.Column(db_.String(100))
     email = db_.Column(db_.String(200))
-    password = db_.Column(db_.String(200))  # there must be hash
+    password = db_.Column(db_.String(255))  # there must be hash
     referral = db_.Column(db_.Integer)
     description = db_.Column(db_.String)
     is_admin = db_.Column(db_.Boolean)
-
-    query: sql.Select
-
-    def __str__(self):
-        return f"<User({self.id=}, {self.full_name=}, {self.username=})>"
